522953c5be8aa60e23215c908b3e02961cba1f18
RocioBurgos31 <117674082+RocioBurgos31@users.noreply.github.com>
2023-02-13 08:47:16 -0600

    Add files via upload

---
+++ src/app/components/componente/componente..components.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+@Component ({
+    selector:  'app-componente',
+    template: ' <h1>Hola desde mi componente <h1>'
+}) 
+export class componenteComponent{
+
+}
